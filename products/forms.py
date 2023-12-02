a3d1f3bd5822195aed3792af245256009815fe46
HinaKhan94 <hinakhan94.hk@gmail.com>
2023-12-02 13:34:34 +0000

    chore: fix image field in add a product with custom widgets

--- products/forms.py
+++ products/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from .models import Product, Category, Review
+from .widgets import CustomClearableFileInput
 from django.core.validators import (
     MaxValueValidator, MinValueValidator
 )
@@ -10,6 +11,8 @@ class ProductForm(forms.ModelForm):
     class Meta:
         model = Product
         fields = '__all__'
+    
+    image = forms.ImageField(label='Image', required=False, widget=CustomClearableFileInput)
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
